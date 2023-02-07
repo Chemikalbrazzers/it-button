b573f6864afa65a817f63aa52e5c543a8cfa6682
Анатолий <98356071+Chemikalbrazzers@users.noreply.github.com>
2023-02-07 05:52:00 +0500

    Add files via upload

---
+++ Button/js/Script.js
@@ -0,0 +1,8 @@
+document.querySelector('#button').addEventListener('click', button)
+
+function button() {
+  console.log(alert('Это моя первая рабочая кнопка мать его'))
+  console.log('Это моя первая рабочая кнопка мать его')
+  
+    
+}
